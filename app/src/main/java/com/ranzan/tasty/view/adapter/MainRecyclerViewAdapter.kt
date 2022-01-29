456f88ee4691b02941bfd0ee410ccc409cd50cc3
RANZN <aryaprakashranjan@gmail.com>
2022-01-29 19:53:25 +0530

    data setting in recycler view

--- app/src/main/java/com/ranzan/tasty/view/adapter/MainRecyclerViewAdapter.kt
+++ app/src/main/java/com/ranzan/tasty/view/adapter/MainRecyclerViewAdapter.kt
@@ -5,21 +5,59 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.ranzan.tasty.databinding.MainItemLayoutBinding
+import com.bumptech.glide.Glide
+import com.ranzan.tasty.databinding.CarouselItemLayoutBinding
+import com.ranzan.tasty.databinding.FeaturedItemLayoutBinding
+import com.ranzan.tasty.databinding.ShoppableItemLayoutBinding
 import com.ranzan.tasty.model.remote.ResultsItem
 
-class MainRecyclerViewAdapter() : RecyclerView.Adapter<MainRecyclerViewHolder>() {
+class MainRecyclerViewAdapter() : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     private var list = ArrayList<ResultsItem>()
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainRecyclerViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val binding = MainItemLayoutBinding.inflate(layoutInflater, parent, false)
-        return MainRecyclerViewHolder(binding)
+    override fun getItemViewType(position: Int): Int {
+        when (list[position].type) {
+            "featured" -> return 1
+            "shoppable_carousel" -> return 2
+            "carousel" -> return 3
+        }
+        return 4
     }
 
-    override fun onBindViewHolder(holder: MainRecyclerViewHolder, position: Int) {
-        holder.setData(list[position])
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        when (viewType) {
+            1 -> {
+                val binding = FeaturedItemLayoutBinding.inflate(layoutInflater, parent, false)
+                return FeaturedViewHolder(binding)
+            }
+            2 -> {
+                val binding = ShoppableItemLayoutBinding.inflate(layoutInflater, parent, false)
+                return ShoppableCarouselViewHolder(binding)
+            }
+            3 -> {
+                val binding = CarouselItemLayoutBinding.inflate(layoutInflater, parent, false)
+                return CarouselViewHolder(binding)
+            }
+            else -> {
+                val binding = CarouselItemLayoutBinding.inflate(layoutInflater, parent, false)
+                return CarouselViewHolder(binding)
+            }
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (getItemViewType(position)) {
+            1 -> {
+                (holder as FeaturedViewHolder).bindData(list[position])
+            }
+            2 -> {
+                (holder as ShoppableCarouselViewHolder).bindData(list[position])
+            }
+            3 -> {
+                (holder as CarouselViewHolder).bindData(list[position])
+            }
+        }
     }
 
     override fun getItemCount(): Int = list.size
@@ -32,19 +70,32 @@ class MainRecyclerViewAdapter() : RecyclerView.Adapter<MainRecyclerViewHolder>()
         diffResult.dispatchUpdatesTo(this)
     }
 
-
 }
 
-class MainRecyclerViewHolder(private val binding: MainItemLayoutBinding) : RecyclerView.ViewHolder(binding.root) {
-    fun setData(data: ResultsItem) {
+class CarouselViewHolder(private val binding: CarouselItemLayoutBinding) : RecyclerView.ViewHolder(binding.root) {
+    fun bindData(data: ResultsItem) {
         binding.tvResultName.text = data.name
         binding.nestedRecyclerView.apply {
             layoutManager = LinearLayoutManager(binding.root.context, LinearLayoutManager.HORIZONTAL, false)
-            if (data.items != null) {
-                adapter = NestedRecyclerViewAdapter(data.items!!)
-            }
+            adapter = NestedRecyclerViewAdapter(data.items!!)
+
         }
+    }
+}
+
+class FeaturedViewHolder(private val binding: FeaturedItemLayoutBinding) : RecyclerView.ViewHolder(binding.root) {
+    fun bindData(resultsItem: ResultsItem) {
+        binding.featuredImage.apply {
+            Glide.with(this).load(resultsItem.item?.thumbnailUrl).into(this)
+        }
+    }
+}
+
+class ShoppableCarouselViewHolder(private val binding: ShoppableItemLayoutBinding) : RecyclerView.ViewHolder(binding.root) {
+    fun bindData(resultsItem: ResultsItem) {
+
     }
 
 }
+
 
